647e3bf5b185a25821ca9ed0a1135579565a428a
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-03-06 16:25:32 -0300

    Implementando a interface

--- interface/interface.py
+++ interface/interface.py
@@ -6,11 +6,42 @@ class Application():
     def __init__(self):
         self.root = root
         self.tela()
+        self.frames_da_tela()
+        self.windgtes_frame1()
         root.mainloop()
     def tela(self):
         self.root.title("SmartClinic - Integrated Clinic Management and Sales System")
         self.root.configure(background="#2A1313")
-        self.root.geometry("800x600")
+        self.root.geometry("700x500")
         self.root.resizable(True, True)
+        self.root.maxsize(width=900, height=700)
+        self.root.minsize(width=400, height=300)
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root, bd = 4, bg="#FFFFFF", highlightbackground="#F9F9F9", highlightthickness=3)
         
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+
+        self.frame_2 = Frame(self.root, bd = 4, bg="#FFFFFF", highlightbackground="#FFFFFF", highlightthickness=3)
+        
+        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
+    def windgtes_frame1(self):
+        #Criação do botao para limpar
+        self.bt_limpar = Button(self.frame_1, text="Limpar")
+        self.bt_limpar.place(relx=0.01, rely=0.01, relwidth=0.1, relheight=0.15)
+        #Criação do botao para buscar
+        self.bt_limpar = Button(self.frame_1, text="Buscar")
+        self.bt_limpar.place(relx=0.12, rely=0.01, relwidth=0.1, relheight=0.15)
+        #Criação do botão novo
+        self.bt_novo = Button(self.frame_1, text="Novo")
+        self.bt_novo.place(relx=0.6, rely=0.01, relwidth=0.1, relheight=0.15)
+        #Criação do botão alterar
+        self.bt_alterar = Button(self.frame_1, text="Alterar")
+        self.bt_alterar.place(relx=0.7, rely=0.01, relwidth=0.1, relheight=0.15)
+        #Criação do botão excluir
+        self.bt_excluir = Button(self.frame_1, text="Excluir")
+        self.bt_excluir.place(relx=0.8, rely=0.01, relwidth=0.1, relheight=0.15)
+        #Criação do label e entrada do codigo
+        self.lb_codigo = Label(self.frame_1, text="Código",)
+        
+
 Application()
